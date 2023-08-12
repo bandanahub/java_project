793ad4365d35614427a4344fb452570b09874cff
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-12 01:47:54 -0400

    Update Main.java
    so i add the code to calculate the age of the employee using current date and the input of the user , previously i had used int as the data type which was not the most desired data type for the date.

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -7,11 +10,9 @@ public class Main {
         System.out.print("Enter the name: ");
         String name = scanner.nextLine();
 
-        System.out.print("Enter the date of birth: ");
-        int DOB = scanner.nextInt();
-
-        System.out.print("Enter the age: ");
-        int age = scanner.nextInt();
+        System.out.print("Enter the date of birth (YYYY-MM-DD): ");
+        String dobInput = scanner.nextLine();
+        int age = calculateAge(dobInput);
 
         System.out.print("Enter the income: ");
         double income = scanner.nextDouble();
@@ -19,12 +20,33 @@ public class Main {
         System.out.print("Enter the occupation rate: ");
         double occ_rate = scanner.nextDouble();
 
-        Employee infemployee = new Employee(name, DOB, age, income, occ_rate);
+        Employee infemployee = new Employee(name, dobInput, age, income, occ_rate);
 
         scanner.close();
 
-
         System.out.println("Employee Information:");
         System.out.println(infemployee.getEmployeeInfo());
+    }
+
+    public static int calculateAge(String dobInput) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dob;
+        try {
+            dob = dateFormat.parse(dobInput);
+        } catch (ParseException e) {
+            System.out.println("Invalid date format. Please use YYYY-MM-DD format.");
+            return -1; // Return a placeholder value to indicate an error
+        }
+
+        Date currentDate = new Date();
+        int years = currentDate.getYear() - dob.getYear();
+        int months = currentDate.getMonth() - dob.getMonth();
+        int days = currentDate.getDate() - dob.getDate();
+
+        if (months < 0 || (months == 0 && days < 0)) {
+            years--;
+        }
+
+        return years;
     }
 }
