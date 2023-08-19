23828acd12f6db2d085c8244cb637e2301417bc6
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-19 01:36:03 -0400

    Update Management.java

--- Management.java
+++ Management.java
@@ -5,93 +5,109 @@ public class Management {
     static double workingDaysPerMonth = 20.0;
 
     List<Employee> employees = new ArrayList<>();
+    // constructor for Motorcycle
+    // make, plate, color, category, sidecar
+    Motorcycle m1 = new Motorcycle("Kawasaki", "Custom Plate", "Yellow", "", false);
+    Motorcycle m2 = new Motorcycle("Honda", "Custom Plate", "Black", "Not for race", true);
 
-    Motorcycle m1 = new Motorcycle("Kawasaki", "Custom Plate", "Yellow", "Race Motorcycle", false);
-    Motorcycle m2 = new Motorcycle("Honda", "Custom Plate", "Black", "Not for Race", true);
+    // constructor for Car
+    // make, plate, color, category, gear, type
+    Car vt1 = new Car("Lamborghini", "Custom Plate", "White", "Family", "Manual", "Sport");
+    Car vt2 = new Car("BMW", "Custom Plate", "Black", "Family", "Automatic", "Sedan");
+    Car vt3 = new Car("Renault Clio", "Custom Plate", "Blue","Family", "Manual", "Hatchback");
+    Car vt4 = new Car("Mazda", "Custom Plate", "White","Family", "Automatic", "SUV");
 
-    Car vt1 = Car("Lamborghini", "Custom Plate", "White", "Family", Gear.Manual, "Sport");
-    Car vt2 = Car("BMW", "Custom Plate", "Black", "Family", Gear.Automatic, "Sedan");
-    Car vt3 = Car("Renault Clio", "Custom Plate", "Blue", "Family", Gear.Manual, "Hatchback");
-    Car vt4 = Car("Mazda", "Custom Plate", "White", "Family", Gear.Automatic, "SUV");
-
+    //    Employee constructors
+    // Manager: name, birthYear, nbClients, nbTravelDays, rate
+    // Programmer: name, birthYear, nbProjects, rate
+    // Tester: name, birthYear, nbBugs, rate
     void hireTest1() {
-        employees.add(Manager("Serge", 1985, 30, 4));
-        employees.add(Manager("Cindy", 1974, 20, 6, 80));
-        employees.add(Programmer("Paul", 1993, 3, 75));
-        employees.add(Tester("Pierre", 1987, 124, 50));
-        employees.add(Programmer("Matt", 1981, 5, 110));
-        employees.add(Programmer("Sarah", 1986, 3));
+        employees.add(new Manager("Serge", 1985, 30, 4));
+        employees.add(new Manager("Cindy", 1974, 20,  80));
+        employees.add(new Programmer("Paul", 1993, 10000, 75, 3));
+        employees.add(new Tester("Pierre", 1987, 12000, 50,50));
+        employees.add(new Programmer("Matt", 1981, 11110, 50, 5));
+        employees.add(new Programmer("Sarah", 1986, 123123,3 , 5));
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
     }
 
+    //    Employee constructors
+    // Manager: name, birthYear, nbClients, nbTravelDays, rate, employeeVehicle
+    // Programmer: name, birthYear, nbProjects, rate, employeeVehicle
+    // Tester: name, birthYear, nbBugs, rate, employeeVehicle
     void hireTest2() {
         employees.clear();
-        employees.add(Manager("Serge", 1985, 30, 4, vt1));
-        employees.add(Manager("Cindy", 1974, 20, 6, 80, vt2));
-        employees.add(Programmer("Paul", 1993, 3, 75, m1));
-        employees.add(Tester("Pierre", 1987, 124, 50, m2));
-        employees.add(Programmer("Matt", 1981, 5, 110, vt4));
-        employees.add(Programmer("Sarah", 1986, 3, vt3));
+        employees.add(new Manager("Serge", 1985, 30, 4, vt1));
+        employees.add(new Manager("Cindy", 1974, 20, 6, vt2));
+        employees.add(new Programmer("Paul", 1993, 3000, 75,1, m1));
+        employees.add(new Tester("Pierre", 1987, 12312, 50,1, m2));
+        employees.add(new Programmer("Matt", 1981, 5000, 110,10,  vt4));
+        employees.add(new Programmer("Sarah", 1986, 1000,3, 1,  vt3));
         for (Employee employee : employees) {
-            System.out.println("-------------------------");
             System.out.println(employee);
-            System.out.println("-------------------------");
-        }
-    }
-
-    void hireTest3() {
-        employees.clear();
-        Employee Serge = Manager("Serge", 1985, 30, 4, vt1);
-        Employee Cindy = Manager("Cindy", 1974, 20, 6, 80, vt2);
-        Employee Paul = Programmer("Paul", 1993, 3, 75, m1);
-        Employee Pierre = Tester("Pierre", 1987, 124, 50, m2);
-        Employee Matt = Programmer("Matt", 1981, 5, 110, vt4);
-        Employee Sarah = Programmer("Sarah", 1986, 3, vt3);
-
-        Contract p1 = Permanent(2, true, 6000, 200, 17);
-        Contract p2 = Permanent(3, false, 5000, 150, 19);
-        Contract t1 = Temporary(60, 30);
-        Contract t2 = Temporary(50, 60);
-        Contract p3 = Permanent(1, true, 4000, 100, 20);
-        Contract p4 = Permanent(4, true, 4500, 100, 19);
-        Serge.signContract(p1);
-        Cindy.signContract(p2);
-        Paul.signContract(t1);
-        Matt.signContract(t2);
-        Pierre.signContract(p3);
-        Sarah.signContract(p4);
-
-        employees.add(Serge);
-        employees.add(Cindy);
-        employees.add(Paul);
-        employees.add(Pierre);
-        employees.add(Matt);
-        employees.add(Sarah);
-
-        for (Employee employee : employees) {
-            System.out.println("-------------------------");
-            System.out.println(employee);
-            System.out.println("-------------------------");
-        }
-
-        System.out.println("-------------------------");
-        System.out.println("Contracts: ");
-        System.out.println("-------------------------");
-        for (Employee employee : employees) {
-            System.out.print(employee.contractInfo());
-        }
-        System.out.println("-------------------------");
-
-        // updating Matt's contract
-        Contract p5 = Permanent(1, true, 5000, 200, 30);
-        Matt.signContract(p5);
-
-        System.out.println("-------------------------");
-        System.out.println("Contracts: ");
-        System.out.println("-------------------------");
-        for (Employee employee : employees) {
-            System.out.print(employee.contractInfo());
         }
         System.out.println("-------------------------");
 
     }
+
+//    void hireTest3() {
+//        employees.clear();
+//        Employee Serge = new Manager("Serge", 1985, 30, 4, vt1);
+//        Employee Cindy = new Manager("Cindy", 1974, 20, 6, 80, vt2);
+//        Employee Paul = new Programmer("Paul", 1993, 3, 75, m1);
+//        Employee Pierre = new Tester("Pierre", 1987, 124, 50, m2);
+//        Employee Matt = new Programmer("Matt", 1981, 5, 110, vt4);
+//        Employee Sarah = new Programmer("Sarah", 1986, 3, vt3);
+
+//        // Permanent: nbChildren, married, monthlySalary, bonusPerChildPerMonth, accumulatedDays
+//        // Temporary: hourlySalary, accumulatedHours
+//        Contract p1 = new Permanent(2, true, 6000, 200, 17);
+//        Contract p2 = new Permanent(3, false, 5000, 150, 19);
+//        Contract t1 = new Temporary(60, 30);
+//        Contract t2 = new Temporary(50, 60);
+//        Contract p3 = new Permanent(1, true, 4000, 100, 20);
+//        Contract p4 = new Permanent(4, true, 4500, 100, 19);
+//        Serge.signContract(p1);
+//        Cindy.signContract(p2);
+//        Paul.signContract(t1);
+//        Matt.signContract(t2);
+//        Pierre.signContract(p3);
+//        Sarah.signContract(p4);
+//
+//        employees.add(Serge);
+//        employees.add(Cindy);
+//        employees.add(Paul);
+//        employees.add(Pierre);
+//        employees.add(Matt);
+//        employees.add(Sarah);
+//
+//        for (Employee employee : employees) {
+//            System.out.println("-------------------------");
+//            System.out.println(employee);
+//            System.out.println("-------------------------");
+//        }
+//
+//        System.out.println("-------------------------");
+//        System.out.println("Contracts: ");
+//        System.out.println("-------------------------");
+//        for (Employee employee : employees) {
+//            System.out.print(employee.contractInfo());
+//        }
+//        System.out.println("-------------------------");
+//
+//        // updating Matt's contract
+//        Contract p5 = new Permanent(1, true, 5000, 200, 30);
+//        Matt.signContract(p5);
+//
+//        System.out.println("-------------------------");
+//        System.out.println("Contracts: ");
+//        System.out.println("-------------------------");
+//        for (Employee employee : employees) {
+//            System.out.print(employee.contractInfo());
+//        }
+//        System.out.println("-------------------------");
+
+//    }
 }
