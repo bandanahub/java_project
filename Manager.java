d85c6883b870cd9ffb3bb1b36d55fbba302fab67
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-19 01:36:52 -0400

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -8,11 +8,22 @@ public class Manager extends Employee {
         this.travelday = travelday;
         this.new_clients = new_clients;
     }
-    public Manager(String name, int birthYear, double income, double occRate, int travelday, int new_clients){
+    public Manager(String name, int birthYear, int new_clients, int travelday){
+        super(name, birthYear, new_clients, travelday);
+
+    }
+
+    public Manager(String name, int birthYear, double income, double occRate, int travelday, int new_clients, String make, String plate, String color, String category){
         super(name, birthYear, income, occRate);
         this.travelday = travelday;
         this.new_clients = new_clients;
     }
+
+    public Manager(String name, int birthYear, int new_clients, int travelday, Car car){
+        super(name, birthYear, new_clients, travelday);
+
+    }
+
     public int getTravelday() {
         return travelday;
     }
@@ -29,10 +40,16 @@ public class Manager extends Employee {
         this.new_clients = new_clients;
     }
 
+    public double annualIncome() {
+        return getIncome() * (12 - (travelday / 20.0)) * (getOccRate() / 100.0);
+    }
+
 
     @Override
     public String toString() {
         return "We have a new employee: " + super.getName() + "," +" a manager.";
+
+
 //                "Age - " + super.getAge()+
 //                super.getName() + "has an employment rate :" + super.getOccRate() + "%" + " travelled " +this.travelday +" days and has brought " +this.new_clients+" clients."+
 //                '}';
