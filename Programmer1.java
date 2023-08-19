069e253f20d67d340ce8fb3e65ef59a131e2d913
Ney_ha <138705103+NehaSardar1@users.noreply.github.com>
2023-08-19 01:34:17 -0400

    Add files via upload

---
+++ Programmer1.java
@@ -0,0 +1,44 @@
+public class Programmer  extends Employee{
+    private int no_projects;
+
+
+    public Programmer() {
+    }
+
+    public Programmer(int no_projects) {
+        this.no_projects = no_projects;
+    }
+
+    public Programmer(String name, int birthYear, double income, double occRate, int no_projects) {
+        super(name, birthYear, income, occRate);
+        this.no_projects = no_projects;
+    }
+
+    public Programmer(String name, int birthYear, double income, double occRate, int no_projects, Car car) {
+        super(name, birthYear, income, occRate);
+        this.no_projects = no_projects;
+    }
+
+    public Programmer(String name, int birthYear, double income, double occRate, int no_projects, Motorcycle motor) {
+        super(name, birthYear, income, occRate);
+        this.no_projects = no_projects;
+    }
+
+    public int getNo_projects() {
+        return no_projects;
+    }
+
+    public void setNo_projects(int no_projects) {
+        this.no_projects = no_projects;
+    }
+
+    @Override
+    public String toString() {
+        return "We have a new employee : " + super.getName() + "," + " a programmer." ;
+//                "Age - " + super.getAge() +
+//                super.getName() + "has an employment rate :" + super.getOccRate() + "%" + " and completed " +this.no_projects +" projects."+
+
+//                '}';
+    }
+}
+
